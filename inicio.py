a8564a6bb31bafc075e82dafaff42e5d88387ea7
ISAAC-RODRIGO <ene88100@gmail.com>
2023-02-24 11:07:53 -0400

    variables

---
+++ inicio.py
@@ -0,0 +1,19 @@
+print(2+3)
+print(3-2)
+print(6*2)
+print(3/9)
+print(5<2)
+print(5>2)
+print(5==2)
+print(5==5)
+print(max("pithon"))
+print(min("python"))
+print(len("python"))
+materia="Programacion Avanzada"
+x=5,5
+y=7
+print(materia)
+nombre=input("ingrese su nombre completo")
+print(nombre)
+edad=input("ingrese su edad")
+print(edad)
