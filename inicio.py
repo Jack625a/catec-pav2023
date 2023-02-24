6d2b6348e72c01f3577c4f8822ff334dd530022d
limberth01 <limberthcopa9@gmail.com>
2023-02-24 15:58:51 +0100

    variables en python

---
+++ inicio.py
@@ -0,0 +1,10 @@
+print(6*5)
+print(7<4)
+materia="programacion avanzada"
+x=5.5
+y=7
+print(materia)
+nombre= input("ingrese su nombre completo")
+print(nombre)
+edad= input("ingrese su edad")
+print(edad)
