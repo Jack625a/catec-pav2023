f9409e63c8d17873c2f0a6b3accf87aaca8267e7
Your Name <nayelivargasalvarado7@gmail.com>
2023-02-27 11:44:36 -0400

    listas y tuplas

---
+++ listas.py
@@ -0,0 +1,20 @@
+#Listas
+colores=['rojo','verde,','amarillo','cafe']
+print ("listas de colores:", colores)
+#update listas
+print (colores[0])
+colores[2]='morado'
+print ("lista nueva: ", colores)
+#Eliminar elementos de una lista
+del colores[1]
+print ("Lista con valor eliminado: " , colores)
+#tuplas
+frutas=('frutilla','banana','uva','pera')
+print (frutas[0])
+verduras=('cebolla','tomate',10)
+tuplafinal=frutas+verduras
+print (tuplafinal)
+listaconvertida=tuple(colores)
+print (listaconvertida)
+tuplanueva=tuplafinal+listaconvertida
+print ("union de lista convertida con tupla", tuplanueva)
