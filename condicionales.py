e48b7b2f696e1dfa3e8ea9ae036c275bc9075b6b
Your Name <nayelivargasalvarado7@gmail.com>
2023-02-28 11:29:16 -0400

    condicinales

---
+++ condicionales.py
@@ -0,0 +1,15 @@
+#ejercicio numero 1 par o impar
+numero= int(input("imgerese numero"))
+if numero%2 ==0:
+    print("el numero ingresado es par")
+else:print("el numero ingresado es impar")
+#Ejercicio 2 - calclar el numero mayor entre dos numeros
+num1=int (input ("ingrese el numero"))
+num2=int(input("ingrese el numero"))
+if num1>num2:
+    print("el numero mayor es: ",num1)
+elif num2>num1:
+    print("el numero mayor es:",num2)
+else:
+    print("os numeros ingresados son iguales")
+    
