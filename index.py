912a602c8dd5f7ca477156a5cd89fdd90ee05eb0
limberth01 <limberthcopa9@gmail.com>
2023-02-28 15:57:10 +0100

    lista y tuplas

--- index.py
+++ index.py
@@ -1,1 +1,20 @@
-print('Hola Mundo')
+#listas
+colores=["rojo","amarrillo","verde","cafe"]
+print("lista de colores: ",colores)
+#update listas
+print(colores[0])
+colores[2]="morado"
+print("lista nueva: ", colores)
+#eliminar elemento de una lista
+del colores[1]
+print("lista con valor eliminado: ", colores)
+#tuplas
+frutas=("frutilla","banana","uva","pera")
+print(frutas[0])
+verduras=("cebolla","tomate",5)
+tuplafinal=frutas+verduras
+print(tuplafinal)
+listaconvertida=tuple(colores)
+print(listaconvertida)
+tuplanueva=tuplafinal+listaconvertida
+print("union de lista convertida con tupla", tuplanueva)
