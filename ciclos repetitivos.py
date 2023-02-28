1c5cb5b385d98c39090219086d03d35b3bf51b00
limberth01 <limberthcopa9@gmail.com>
2023-02-28 17:14:33 +0100

    ciclos repetitivos

---
+++ ciclos repetitivos.py
@@ -0,0 +1,24 @@
+#ejercicio 1 - numeros del 1 al 100
+numeros=1
+while numeros<=1:
+    print(numeros)
+    numeros+=1
+#numeros pares hasta del 1 al 100
+pares=2
+while pares<=100:
+    print(pares)
+    pares+=2
+#numeros del 10 al 1
+num1=10
+while num1>=1:
+    print(num1)
+    num1-=1
+#serie fibonaci
+x=0
+y=1
+q=1
+while q<=10:
+    print(y)
+    x+y==x
+    y=y
+    q+=1
