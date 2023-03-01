37c5b0da91df8dd071cfae4aec957224636dfdd7
brandon <brandonfex@outlook.com>
2023-03-01 12:16:58 -0400

    ciclo ford

---
+++ cicloford.py
@@ -0,0 +1,24 @@
+suma=0
+for i in range(1,10):
+    suma +=i
+    print("la suma total es :", suma)
+
+#impar
+
+inpar=1
+for i in range(1,11):
+    print(inpar)
+    inpar +=1
+
+# fibonaci con for
+print("serie fibonaci ciclo ford")
+a=0
+b=1
+resultado=1
+for i in range(1,11):
+    print(a)
+    a=b
+    b=resultado
+    resultado=a+b
+
+
