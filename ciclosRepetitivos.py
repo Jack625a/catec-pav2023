3ca507ba6a3e2e8b2369c3e2a12b5876c8fcee30
brandon <brandonfex@outlook.com>
2023-03-01 11:07:30 -0400

    ciclosrepetitivos2

---
+++ ciclosRepetitivos.py
@@ -0,0 +1,40 @@
+numeros =1
+while numeros<=10:
+    print(numeros)
+    numeros+=1
+
+#numeros pares del 1 al 100
+print("numeros par del 1 al 100 ")
+pares = 2
+while pares <=100:
+    print("numeros pares ",pares)
+    pares+=2
+
+#numeros impares
+print("numeros impares")
+inpares = 10
+while inpares >=1:
+    print("numeros inpares ",inpares)
+    inpares-=1
+
+#serie de fibonaci
+print(" serie fibonaci")
+
+x=0
+y=1
+z=1
+sumatoria=1
+while(z<=10):
+    z +=1
+    print(x)
+    x=y
+    y=sumatoria
+    sumatoria=x+y
+
+
+
+
+
+
+
+
