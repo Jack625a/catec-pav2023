387b3730bcb02327e1b2bc6ec567480a7a34dab6
limberth01 <limberthcopa9@gmail.com>
2023-03-01 17:16:59 +0100

    ciclo for

---
+++ ciclofor.py
@@ -0,0 +1,41 @@
+suma=0
+for i in range(1,10):
+    suma+=i
+    print("la suma total es: ",suma)
+#impar
+impares=1
+for i in range(1,10,2):
+    impares +=i
+    print(impares)
+#lista de 1 al 10 
+pares=2
+while pares<=100:
+    print(pares)
+    pares+=2
+#lista de 1 al 10 en for
+pares=2
+for i in range(1,10):
+    print(pares)
+    pares+=2
+#serie fibonaci
+print("serie fibonaci")
+x=0
+y=1
+q=1
+sumatoria=1
+while (q<=10):
+    q +=1
+    print(x)
+    x=y
+    y=sumatoria
+    sumatoria=x+y
+#serie fibonaci en for
+a=0
+b=1
+n=1
+resultado=1
+for i in range(1,11):
+    print(a)
+    a=b
+    b=resultado
+    resultado=a+b
