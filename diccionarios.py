5da485aba39728edd4e3da9c0c2aa88f040bd121
brandon <brandonfex@outlook.com>
2023-03-01 11:30:14 -0400

    diccionarios

---
+++ diccionarios.py
@@ -0,0 +1,10 @@
+
+estudiante={"nombre":"Brandon","Edad":"27","CI":"754585","carrera":"sistemas informaticos"}
+print(estudiante)
+print(estudiante["nombre"])
+print(estudiante["carrera"])
+print(estudiante["CI"])
+estl=(estudiante["nombre"])
+print(estl)
+estl="mario"
+print(estl)
