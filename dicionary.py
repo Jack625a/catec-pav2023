341ccf99e14610a7a814baf03baa8f2cb9fc3b50
limberth01 <limberthcopa9@gmail.com>
2023-03-01 16:30:25 +0100

    Ddicionario

---
+++ dicionary.py
@@ -0,0 +1,9 @@
+#
+estudiante={"Nombre":"Copa","Edad":"18","CI":"8347759","Carrera":"Sistemas infirmaticos"}
+print(estudiante)
+print(estudiante["Nombre"])
+print(estudiante["Carrera"])
+est1=(estudiante["Nombre"])
+print(est1)
+est1="juan"
+print(est1)
