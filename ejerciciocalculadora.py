bcdbd6cfdc3c151e4240e4fbef2149c7c1d93a2b
limberth01 <limberthcopa9@gmail.com>
2023-03-08 16:57:45 +0100

    funciones matematicas

---
+++ ejerciciocalculadora.py
@@ -0,0 +1,8 @@
+import math
+print("calculadora pav-2023")
+a=0
+b=0
+print("seleccione las operaciones que desea realizar")
+print("operaciones aritmeticas= oa")
+print("operaciones trigonometricas= ot")
+
---
+++ funciones matematicas.py
@@ -0,0 +1,7 @@
+import math
+x=float(input("ingrese un numero"))
+print("el seno de x es: ", math.sin(x))
+print("el tangente de x es:", math.tan(x))
+print("el coseno de x es:", math.cos(x))
+print("la raiz cuadrada de ", x; " es "math.sqrt(x))
+
