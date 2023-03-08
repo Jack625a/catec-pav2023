8c3e312409c6da3f1b96bd4f67a093c5f57d721e
Your Name <nayelivargasalvarado7@gmail.com>
2023-03-08 11:20:02 -0400

    funciones con argumentos

---
+++ funciones con argumentos.py
@@ -0,0 +1,25 @@
+#funciones con argumrntos
+def saludar(nombre, mensaje="hola bienvenido"):
+            print( mensaje +" "+ nombre)
+
+saludar("kevin")
+
+saludar("kevin")
+saludar("kevin","Buenos dias")
+
+#funciones Lambda
+suma= lambda x,y:x+y
+print(" la suma es:", suma(7,2))
+resta= lambda x,y:x-y
+print(" la resta es:", resta(7,2))
+multiplicar= lambda x,y:x*y
+print(" la multiplicacion es:", multiplicar(7,2))
+dividir= lambda x,y:x/y
+print(" la divicion es:", dividir(7,2))
+
+#ordenar una lista
+numeros=(45,4,8,42,13,1,69,78,5,17,6)
+ordenar=sorted(numeros,key=lambda x:x)
+print (ordenar)
+pares= list(filter(lambda x:x % 2==0,numeros))
+print(pares)
