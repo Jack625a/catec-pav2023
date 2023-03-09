4d489fa12eddeeb77e64380dfb31b1034ab36175
brandon <brandonfex@outlook.com>
2023-03-09 11:49:26 -0400

    calculadora

---
+++ funcionesmatematicas.py
@@ -0,0 +1,8 @@
+import math 
+x=float(input("ingrese un numero: "))
+print("el seno es: ", math.sin(x))
+print("el tangente es: ", math.tan(x))
+print("el coseno es: ", math.cos(x))
+
+print("la raiz cuadrada es: ", math.sqrt(x))
+
