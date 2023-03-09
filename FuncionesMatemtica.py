e6e851216e07847c4046e6fdf90acb2966f0088e
Luchini <ferb1683@gmail.com>
2023-03-09 16:46:31 +0100

    Calculadora

---
+++ FuncionesMatemtica.py
@@ -0,0 +1,6 @@
+import math 
+x=float(input("Ingrese un numero"))
+print("El seno de x es:", mat.sin(x))
+print("El tangente de x es:", mat.tan(x))
+print("El coseno de x es:", mat.cos(x))
+print("La raiz cuadrada de " ,x ,"es:", math.sqrt(x))
