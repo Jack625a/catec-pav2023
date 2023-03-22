1f9aa05c5803c5738441a1cf5e7e2aa4cac1c30f
limberth01 <limberthcopa9@gmail.com>
2023-03-22 17:14:30 +0100

    todos los comandos

---
+++ chat_bot_oficial.py
@@ -0,0 +1,22 @@
+import pyttsx3
+import openai
+import webbrowser
+import speech_recognition as sr
+import pyaudio
+
+#creacion de funciones
+teclado
+#primera opciion del menu
+def chatbot{}:
+print("chatbot")
+#segunda opcion del menu
+def chatbot_voz():
+    print("chatbot mediante vos")
+def calculadora():
+    print("calculadora avanzada")
+def musica():
+    print("musica")
+def asistente():
+    print("asistente que navega por internet")
+
+#funciones especiales
