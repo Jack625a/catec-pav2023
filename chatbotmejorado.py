f602da9400eb09f21164ac2b3cc866f5cf19e4a4
limberth01 <limberthcopa9@gmail.com>
2023-03-22 16:48:36 +0100

    chatmejorado

--- chatbotmejorado.py
+++ chatbotmejorado.py
@@ -1,13 +1,13 @@
 import openai
 import time
-openai.api_key="sk-rdgfhghgd"
+openai.api_key="sk-"
 modelo="text-davinci-003"
 
 print("Hola bienvenido mi nombres PAV estoy aqui para resolver cualquier pregunta que tengas ¿En que puedo ayudarte? ")
 
 def obtener_respuesta(pregunta):
     respuesta=openai.Completion.create(
-        engine="text-davinci-003",
+        engine=modelo,
         prompt=pregunta,
         temperature=0.5,
         max_tokens=100,
@@ -16,3 +16,9 @@ def obtener_respuesta(pregunta):
         presence_penalty=0
     )
     return respuesta.choices[0].text.strip()
+
+while True:
+    pregunta=input("cual es tu consulta que deseas resolver? : ")
+    repuesta=obtener_respuesta(pregunta)
+    print("la respuesta a la pregunta: ",pregunta,"es: ",repuesta)
+    time.sleep(1)
